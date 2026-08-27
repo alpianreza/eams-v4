00d3764d30e7f241c1e4940b682691aa6cf131c9
Reza Alpian <93972158+alpianreza@users.noreply.github.com>
2026-08-28 06:45:23 +0700

    fix(ui): avoid Blade error directive collision

--- resources/views/components/ui/image-preview.blade.php
+++ resources/views/components/ui/image-preview.blade.php
@@ -2,7 +2,7 @@
 
 <div x-data="{ failed: false }" {{ $attributes->class('eams:relative eams:overflow-hidden eams:rounded-eams-lg eams:border eams:border-border eams:bg-surface-sunk') }} data-eams-component="image-preview">
     @if($src)
-        <img x-show="! failed" @error="failed = true" src="{{ $src }}" alt="{{ $alt }}"
+        <img x-show="! failed" x-on:error="failed = true" src="{{ $src }}" alt="{{ $alt }}"
              @class(['eams:w-full eams:object-cover', 'eams:aspect-video' => $ratio === 'video', 'eams:aspect-square' => $ratio === 'square'])>
         <div x-cloak x-show="failed"
              @class(['eams:flex eams:w-full eams:flex-col eams:items-center eams:justify-center eams:p-6 eams:text-center eams:text-muted', 'eams:aspect-video' => $ratio === 'video', 'eams:aspect-square' => $ratio === 'square'])>
